7538119684e7935bab7b143754e70d8288d5d833
programjm <programjm@gmail.com>
2019-08-12 13:51:04 -0500

    Refactor theme file to use typescript

---
+++ src/styled.d.ts
@@ -0,0 +1,22 @@
+import 'styled-components'
+
+// Extend the styled components type declarations to pass the theme easily
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        typography: {
+            fonts: {
+                title: string
+            },
+            sizes: {
+                h1: string
+                h2: string
+            }
+        }
+        color: {
+            bg: {
+                primary: string
+                secondary: string
+            }
+        }
+    }
+}
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import {DefaultTheme} from 'styled-components'
+
+export const theme: DefaultTheme = {
+    typography: {
+        fonts: {
+            title: "sans-serif",
+        },
+        sizes: {
+            h1: "32px",
+            h2: "16px",
+        }
+    },
+    color: {
+        bg: {
+            primary: "#ffffff",
+            secondary: "#ccffd8"
+        }
+    }
+};
