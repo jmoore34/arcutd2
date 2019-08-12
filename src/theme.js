381350aea36d756684459c9f44f3e50718668f3d
programjm <programjm@gmail.com>
2019-08-12 13:00:19 -0500

    Add theme.js

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export default {
+    typography: {
+        fonts: {
+            title: "sans-serif",
+        },
+        sizes: {
+            h1: "32px",
+            h2: "16px",
+        }
+    },
+
+};
