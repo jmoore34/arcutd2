fd15b983f6272c398e5526339e6544560cb7dfbc
programjm <programjm@gmail.com>
2019-08-12 18:25:37 -0500

    Add Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const defaultColor = "#ffffff"
+
+export const Button =  styled.div<{color?: string}>`
+  border: 1px solid;
+  border-color: ${(props) => props.color || defaultColor}
+  border-radius: 100px;
+  padding: 5px 20px;
+  margin: 15px;
+  text-align: center;
+  cursor: pointer;
+  pointer-events: auto;  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  & {
+    color: ${(props) => props.color || defaultColor};
+    flex: 0 1 auto;
+    font-size: 30px;
+    font-family: ${(props) => props.theme.typography.fonts.title}; 
+  } 
+  
+  &:hover {
+    background-color: rgba(101, 195, 70, 0.54)
+  }
+  transition: all 0.2s;
+`;
