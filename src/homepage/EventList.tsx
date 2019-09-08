fa3f586355550dd02bb95c384626f234afbd1651
programjm <programjm@gmail.com>
2019-09-08 18:21:42 -0500

    Implement EventList

--- src/homepage/EventList.tsx
+++ src/homepage/EventList.tsx
@@ -1,23 +1,55 @@
 import styled from 'styled-components'
 import * as React from 'react'
 import {Event} from 'events'
+import {getShortDayName, getShortMonthName} from "../util/TimeUtil";
+import {events} from "../events";
+import {LI, Text} from '../components/typography';
 
 // Dims (lowers opacity) of contents on demand
 const Dimmer = styled.div<{dim: boolean}>`
   opacity: ${props => props.dim ? 0.6 : 1.0}
 `;
 
+const EventName = styled(Text)`
+  font-weight: bold;
+  color: black;
+`;
+
+const EventDetails = styled(Text)`
+  color: rgba(22,40,26,0.96);
+`;
+
+const EventDesc = styled(Text)`
+  font-style: italic;
+  color: #212923;
+`;
+
+
 const SingleEvent: React.FunctionComponent<{event: Event}> = (props) => {
     // use different styling if event has passed
-    const now = 
-    const past: boolean = props.event.year <
-    return <Dimmer dim={past}>
+    const dayAfterEvent: Date = new Date(props.event.date.getTime() + 24*60*60*1000);
+    const passed = dayAfterEvent < new Date(); // event was before now. We actually use the day after the event so that the event doesn't show as passed when the time is greater than midnight
+    return <Dimmer dim={passed}>
+            <LI>
+                <EventName>{props.event.name} </EventName>
+                <EventDetails>{`(${getShortDayName(props.event.date)}. ${getShortMonthName(props.event.date)}. ${props.event.date.getDate()}, ${props.event.time} @ ${props.event.location}) `}</EventDetails>
+                <EventDesc>{props.event.description}</EventDesc>
+            </LI>
+        </Dimmer>;
+};
 
-    </Dimmer>;
-}
+const EventContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: square;
+`;
 
 export class EventList extends React.Component<{},{}> {
     render() {
-        return <></>
+        return <EventContainer>
+            {events.map((event,  id)=>
+                <SingleEvent key={id} event={event}/>
+        )}
+        </EventContainer>
     }
 }
