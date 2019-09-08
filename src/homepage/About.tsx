9db8566be1a9c289c3e6c80e50cbb19e505b1125
programjm <programjm@gmail.com>
2019-09-07 22:29:25 -0500

    Remove use of Section from About

--- src/homepage/About.tsx
+++ src/homepage/About.tsx
@@ -1,14 +1,28 @@
-import {Section} from "../components/Section";
 import {H1, H2, Paragraph} from '../components/typography';
 import * as React from "react";
+import styled from "styled-components";
 
-export const About: React.FunctionComponent = (props) => <Section odd>
-    <H1>About us</H1>
-    <Paragraph>The Animal Rights Club at UT Dallas is a student organization committed to providing education about
-        animal rights issues, volunteering on and off campus, and building a sense of community for those who care
-        about animals. General meetings are typically held every other Wednesday at 7PM in RHS. We also host a number
-        of internal events ranging from film screenings to cooking socials to VR outreach tabling. Moreover, the club
-        facilitates carpooling to external volunteer events in the DFW area.
-    </Paragraph>
-</Section>;
+const MainDiv = styled.div`
+  flex: 1 0 auto;
+  //height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 40px;
+  margin: 40px 50px;
+`;
+
+const Section = styled.div``;
+
+export const About: React.FunctionComponent = (props) => <MainDiv>
+    <Section>
+        <H1>About us</H1>
+        <Paragraph>The Animal Rights Club at UT Dallas is a student organization committed to providing education about
+            animal rights issues, volunteering on and off campus, and building a sense of community for those who care
+            about animals. General meetings are typically held every other Wednesday at 7PM in RHS. We also host a number
+            of internal events ranging from film screenings to cooking socials to VR outreach tabling. Moreover, the club
+            facilitates carpooling to external volunteer events in the DFW area.
+        </Paragraph>
+    </Section>
+</MainDiv>;
 
