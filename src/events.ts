ea6ade3b5b1ac647126ea66700c8625b873b6d2f
programjm <programjm@gmail.com>
2019-09-08 18:05:42 -0500

    Add events.ts

--- src/events.ts
+++ src/events.ts
@@ -1,15 +1,21 @@
+import {mdyDate} from "./util/TimeUtil";
+
 export interface Event {
-    month: number,
-    day: number,
-    year: number,
+    date: Date,
     time: string,
     location: string,
     name: string,
     description: string,
-    url: string
+    url?: string
 }
 
 export const events: Event[] = [
-    {month: 8, day: 12, year: 2019, time: "3PM", location: "Main Street Garden Park", name: "Official Animal Rights March", description: "The most important external event of the year. Join us to raise awareness of the most pressing animal rights issues.", url:"https://www.facebook.com/events/321824155189732/" },
-
+    {date: mdyDate(8,12,2019), time: "3PM", location: "Main Street Garden Park", name: "Official Animal Rights March", description: "The most important external event of the year. Join us to raise awareness of the most pressing animal rights issues.", url:"https://www.facebook.com/events/321824155189732/" },
+    {date: mdyDate(9,6,2019), time: "7PM", location: "Phase 8 Clubhouse", name: "Back to School Pool Party", description: ""},
+    {date: mdyDate(9,11,2019), time: "7PM", location: "RHS 1.001", name: "Pin Making Night", description: ""},
+    {date: mdyDate(9,25,2019), time: "7PM", location: "RHS 1.001", name: "Movie Night", description: ""},
+    {date: mdyDate(10,9,2019), time: "7PM", location: "RHS 1.001", name: "Vegan Baking Social", description: ""},
+    {date: mdyDate(10,23,2019), time: "7PM", location: "RHS 1.001", name: "Homecoming Parade Poster Making", description: ""},
+    {date: mdyDate(11,6,2019), time: "7PM", location: "RHS 1.001", name: "Activist Letter Writing", description: ""},
+    {date: mdyDate(11,20,2019), time: "7PM", location: "RHS 1.001", name: "Vegan Thanksgiving Potluck", description: ""},
 ];
