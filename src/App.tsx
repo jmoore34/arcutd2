871196d4e354f7d53a9918d04c23bac4b350842f
programjm <programjm@gmail.com>
2019-09-08 22:30:30 -0500

    Add styled-normalize

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {theme} from 'theme'
 import {ThemeProvider} from 'styled-components'
 import {Home} from 'homepage/Home'
+import {Normalize} from "styled-normalize";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+      <>
+        <Normalize />
+        <ThemeProvider theme={theme}>
+          <Home />
+        </ThemeProvider>
+      </>
   );
 }
 
